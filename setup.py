9d3ccb6a40ce15c01c6f51d0e9496a901cd93ef4
Simon Zimmermann <mail@simonzimmermann.de>
2024-03-27 11:53:32 +0100

    rename package

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ from os import getenv
 version = getenv("PYTHON_SOFA_VERSION", "0.0.0")
 
 setup(
-    name="python-sofa",
+    name="dr-python-sofa",
     version=version,
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=["numpy", "scipy>=1.2.0", "netcdf4", "datetime"],
     author="Jannika Lossner",
     author_email="jnlossner@gmail.com",
-    description="Python SOFA API",
+    description="Python SOFA API (Dear Reality Fork)",
     long_description=open("README.rst").read(),
     license="MIT",
     keywords="audio SOFA acoustics".split(),
-    url="http://github.com/spatialaudio/python-sofa/",
+    url="http://github.com/dearreality/python-sofa/",
     platforms="any",
     python_requires=">=3.5",
     classifiers=[
